1162803279bebfeef28f152395758b22c87589f6
oranheim <oranheim@gmail.com>
2019-10-15 09:17:40 +0200

    Renamed docker labels

--- tools/docker-postgres-init-db.bash
+++ tools/docker-postgres-init-db.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker cp init-db-and-user.sql $(docker ps -qf "name=be-data-collection-consumers_dcPostgresDb_1"):/root/init-db-and-user.sql
+docker cp init-db-and-user.sql $(docker ps -qf "name=be-data-collection-consumers_dc-postgres-db_1"):/root/init-db-and-user.sql
 
-docker exec -i $(docker ps -qf "name=be-data-collection-consumers_dcPostgresDb_1") psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres -f /root/init-db-and-user.sql
+docker exec -i $(docker ps -qf "name=be-data-collection-consumers_dc-postgres-db_1") psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres -f /root/init-db-and-user.sql
